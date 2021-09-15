e4a22037d2862ae6e8f49bf7536a50c1c830a5f1
Mark Heijnekamp <mark@heijnekamp.nl>
2021-09-15 18:16:25 +0200

    Working on 2-15

---
+++ MIC/tasker/2-15/build/CMakeFiles/disasm.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for disasm.
+# This may be replaced when dependencies are built.
---
+++ MIC/tasker/2-15/src/main.cpp
@@ -0,0 +1,44 @@
+// keep intellisense satisfied
+#ifndef __AVR_ATmega328P__
+#define __AVR_ATmega328P__
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <HardwareSerial.h>
+#include <util/delay.h>
+
+
+int main()
+{
+
+  Serial.begin(9600);
+  // BUTTON
+  DDRB &= ~(1 << DDB1);
+  PORTB = (1<<PORTB1);
+  // LED
+  DDRC |= (1 << DDC5);
+  while (1)
+  {
+    		_delay_ms(500);
+
+    Serial.print(PORTB  & (1<<PORTB1));
+    Serial.print((!(~(PORTB | ~((1 << PORTB1))))));
+
+    Serial.print(PINB & (1<<PINB1) == 0);
+    Serial.print(!(PINB & (1<<PINB1)));
+    Serial.print(PINB & (1<<PINB1));
+    Serial.print(PINB & (1<<PINB1) == (1<<PINB1));
+    if ((!(~(PORTB | ~((1 << PORTB1))))))
+    {
+      PORTC ^= (1 << PORTC5);
+      Serial.println("BUTTON PRESSED");
+    }
+    else
+    {
+      PORTC &= ~(1 << PORTC5);
+      Serial.println("BUTTON DEPRESSED");
+
+    }
+  }
+}
